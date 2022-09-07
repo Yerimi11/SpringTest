fd31cc200a0e94828c2e63946112426bdf077a59
yerim <sontt4266@naver.com>
2022-09-07 22:35:32 +0900

    new start

--- src/main/java/com/example/restaurant/controller/ApiController.java
+++ src/main/java/com/example/restaurant/controller/ApiController.java
@@ -16,31 +16,11 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ApiController {
     
-    private final WishListService wishListService;
+//    private final WishListService wishListService;
 
     @GetMapping("/search")
     public WishListDto search(@RequestParam String query){
-        return wishListService.search(query);
+//        return wishListService.search(query);
     }
 
-    @PostMapping("")
-    public WishListDto add(@RequestBody WishListDto wishListDto){
-        log.info("{}", wishListDto);
-        return wishListService.add(wishListDto);
-    }
-
-    @GetMapping("/all")
-    public List<WishListDto> findAll(){
-        return wishListService.findAll();
-    }
-
-    @DeleteMapping("/{index}")
-    public void delete(@PathVariable int index){
-        wishListService.delete(index);
-    }
-
-    @PostMapping("/{index}")
-    public void addVisit(@PathVariable int index){
-        wishListService.addVisit(index);
-    }
 }
